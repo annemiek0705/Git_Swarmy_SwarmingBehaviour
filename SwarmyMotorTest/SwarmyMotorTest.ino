ec399f78102f804b93b6a5c9df2383e848cf2c57
Annemiek Fokke <aaamiek@gmail.com>
2022-10-24 16:34:15 +0200

    Added reverse motor

--- SwarmyMotorTest/SwarmyMotorTest.ino
+++ SwarmyMotorTest/SwarmyMotorTest.ino
@@ -12,19 +12,29 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bot.OLEDprint("Motortest", "Linker Motor vooruit", "Index 0, Speed 100");
+  bot.OLEDprint("Motortest", "Draai naar links", "Index 0, Speed 100");
   bot.setMotorSpeed(0, 100);
   bot.setMotorSpeed(1, 0);
-  delay(1000);
-
-  bot.OLEDprint("Motortest", "Rechter motor vooruit", "Index 1, Speed 100");
-  bot.setMotorSpeed(0, 0);
+  delay(50);
+  bot.setMotorSpeed(0, 100);
   bot.setMotorSpeed(1, 100);
   delay(1000);
 
-  bot.OLEDprint("Motortest", "Beide motoren", "Speed 100");
+  bot.OLEDprint("Motortest", "Draai naar rechts", "Index 1, Speed 100");
+  bot.setMotorSpeed(0, 0);
+  bot.setMotorSpeed(1, 100);
+  delay(50);
+  bot.setMotorSpeed(0, 100);
+  bot.setMotorSpeed(1, 100);
+  delay(1000);
+
+  bot.OLEDprint("Motortest", "Beide motoren vooruit", "Speed 100");
   bot.setMotorSpeed(0, 100);
   bot.setMotorSpeed(1, 100);
   delay(1000);
   
+  bot.OLEDprint("Motortest", "Beide motoren achteruit", "Speed 100");
+  bot.setMotorSpeed(0, -100);
+  bot.setMotorSpeed(1, -100);
+  delay(1000);
 }
