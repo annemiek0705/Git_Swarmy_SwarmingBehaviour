efc7adb40db9f8fc06eb2b4c25e312de05b4363b
Annemiek Fokke <aaamiek@gmail.com>
2022-10-31 11:32:45 +0100

    IR Reading all sensors

--- InfraroodTest/IR_Receiver/IR_Receiver.ino
+++ InfraroodTest/IR_Receiver/IR_Receiver.ino
@@ -2,8 +2,9 @@
 #include <Swarmy.h>
 
 Swarmy bot(false);
-int amplitude;
-int frequency;
+int amplitude[8];
+int frequency[8];
+int i;
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,9 +18,13 @@ void loop() {
   bot.OLEDprint("", "Receive IR", "");
   bot.ReceiveIR();
   bot.OLEDprint("", "get amplitude", "");
-  amplitude = bot.getAmplitude(5);
-  frequency = bot.getFrequency(5);
+  for(i = 0; i < 8; i++){
+    amplitude[i] = bot.getAmplitude(i);
+    frequency[i] = bot.getFrequency(i);
+  }
+  String ampStr = "Amplitude| 0: "+ String(amplitude[0])+ "1:"+String(amplitude[1])+ "2:"+String(amplitude[2])+"3:"+ String(amplitude[3])+"4:"+String(amplitude[4])+"5:"+String(amplitude[5])+"6:"+String(amplitude[6])+"7:"+String(amplitude[7]) ;
+  String freqStr = "Frequency| 0: " + String(frequency[0])+ "1:"+String(frequency[1])+ "2:"+String(frequency[2])+"3:"+String(frequency[3])+"4:"+String(frequency[4])+"5:"+String(frequency[5])+"6:"+String(frequency[6])+"7:"+String(frequency[7]) ;
   
-  bot.OLEDprint(String(amplitude), String(frequency), "");
+  bot.OLEDprint(ampStr, freqStr, "");
   delay(500);
 }
