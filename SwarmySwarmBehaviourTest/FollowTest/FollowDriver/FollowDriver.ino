861ccc1e70939d71be17e61cef9f04bc20a0c80d
Annemiek Fokke <aaamiek@gmail.com>
2022-12-08 17:52:17 +0100

    Motor_AmplitudeSpeedTest
    Implemented a sigmoid function into the motor to determine the speed in combination with the amplitude. I used an inverse sigmoid function for this.

--- SwarmySwarmBehaviourTest/FollowTest/FollowDriver/FollowDriver.ino
+++ SwarmySwarmBehaviourTest/FollowTest/FollowDriver/FollowDriver.ino
@@ -5,24 +5,25 @@
 Swarmy bot(false);
 unsigned long startMillis;  //some global variables available anywhere in the program
 unsigned long currentMillis;
-long period = 1000;  //the value is a number of milliseconds
+long period = 5000;  //the value is a number of milliseconds
 int count = 1;
+
 void setup() {
   // put your setup code here, to run once:
   bot.OLEDprint("", "Driver!", "");
   startMillis = millis();  //initial start time
-  bot.setInfraredFrequency(5000);
+  bot.setInfraredFrequency(1000);
 }
 
 void loop() {
   currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
   if (currentMillis - startMillis >= period){
     if(count % 2){
-      bot.setMotorSpeed(0, -100);
-      bot.setMotorSpeed(1, -100); 
+     // bot.setMotorSpeed(0, -50);
+     // bot.setMotorSpeed(1, -50); 
     }else{
-      bot.setMotorSpeed(0, 100);
-      bot.setMotorSpeed(1, 100);
+     // bot.setMotorSpeed(0, 50);
+     // bot.setMotorSpeed(1, 50);
     }
     count++;
     startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
--- SwarmySwarmBehaviourTest/FollowTest/FollowTest.ino
+++ SwarmySwarmBehaviourTest/FollowTest/FollowTest.ino
@@ -1,140 +1,194 @@
 #include <config.h>
 #include <Swarmy.h>
+#include <math.h>
 #include <arduino-timer.h>
 
 Swarmy bot(false);
-typedef enum{Searching, Following}states;
-states state = Searching;
-int amplitude[8], frequency[8] ,foundFreq[8], initAmp[8];
-bool swarmyFound = 0;
-unsigned long startMillis;  //some global variables available anywhere in the program
+
+typedef enum{SEARCHING, FOLLOWING} BEHAVIOUR_STATES;
+typedef enum{NONE, FORWARD, BACKWARD, LEFT, RIGHT, FORWARD_LEFT, FORWARD_RIGHT} MOTOR_DIRECTION;
+
+MOTOR_DIRECTION currentDirection = NONE;
+BEHAVIOUR_STATES state = SEARCHING;
+
+int amplitude[8], frequency[8];
+
+int ownFrequency = 2000;
+int targetFrequency = 1000;
+int threshold = 25;
+
 unsigned long currentMillis;
-const unsigned long period = 1000;  //the value is a number of milliseconds
+unsigned long period = 1000;  //the value is a number of milliseconds
+unsigned long startMillis;  //some global variables available anywhere in the program
+static float desiredAmp = 150;
+const float e = 2.71828;
 
 void setup() {
   // put your setup code here, to run once:
   bot.OLEDprint("", "Starting", "");
   startMillis = millis();  //initial start time
   Serial.begin(115200);
+  bot.setInfraredFrequency(ownFrequency);
 }
 
 void loop() {
   switch(state){
-    case Searching:
-      bot.OLEDprint("", "Searching", "");
+    case SEARCHING: {
+      //bot.OLEDprint("", "Searching", "");
       currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
-      if (currentMillis - startMillis >= period){
-        driving();
-        startMillis = currentMillis;  //IMPORTANT to save the start time of the current LED state.
-      }
-  
-      if(receiveIR()==1000 && swarmyFound == 0){
-        swarmyFound = 1;
-        state = Following;  
-        bot.OLEDprint(String(frequency[0])+" " + String(frequency[1])+" "+ String(frequency[2]), String(frequency[3])+" "+ String(frequency[4])+ " "+String(frequency[5]),String(frequency[6])+" "+String(frequency[7]));  
-      }
-    break;
-    
-    case Following:
-      adjustDirection(determineDirection());
-      bot.OLEDprint(String(amplitude[0])+" " + String(amplitude[1])+" "+ String(amplitude[2]), String(amplitude[3])+" "+ String(amplitude[4])+ " "+String(amplitude[5]),String(amplitude[6])+" "+String(amplitude[7]));  
       
-      if(receiveIR()!=1000){
-        swarmyFound = 0;
-        state = Searching;  
-      } 
-      break;
+      if (currentMillis - startMillis >= period){  
+        searchPattern();
+        startMillis = currentMillis; 
+      }
+
+      int irFreq = receiveIR();
+      int irAmp = determineAmplitude();
+      if(irFreq > (targetFrequency - threshold) && irFreq < (targetFrequency + threshold)){
+        state = FOLLOWING;    
+      }
+      bot.OLEDprint("Frequency: "+ String(irFreq),"Amplitude: " +String(irAmp),"");
+      driving();
+    }
+    break;
+    case FOLLOWING: {
+      int irFreq = receiveIR();
+      if(!(irFreq > (targetFrequency - threshold) && irFreq < (targetFrequency + threshold))){  
+        state = SEARCHING;    
+      }
+
+      adjustDirection(determineDirection());  
+      driving();
+      
+    }
+    break;
   }
 }
 
 int receiveIR(){
-  int freqFound;
+  int freqFound, ampFound;
   bot.ReceiveIR();
   for(int i = 0; i < 8; i++){
     amplitude[i] = bot.getAmplitude(i);
     frequency[i] = bot.getFrequency(i);
     if(frequency[i]>500){
-      Serial.println("Frequency found");
       freqFound = frequency[i];
+      ampFound = amplitude[i];
     }
   }
   return freqFound;
 }
-int determineAmp(){
-  int highestAmplitude = 0;
-  for(int i=0; i<8;i++){
-    if(highestAmplitude<amplitude[i]){
-      highestAmplitude = amplitude[i];  
-    }
-  }
-  return highestAmplitude;
-}
 
-int adjustPosition(int foundAmp){
-  if(foundAmp<98){
-    //driveforward
-    bot.setMotorSpeed(0, -100);
-    bot.setMotorSpeed(1, -100); 
-  }else if(foundAmp>100){
-    //drivebackward
-    bot.setMotorSpeed(0, 100);
-    bot.setMotorSpeed(1, 100); 
-  }
+int determineAmplitude(){
+  int highestAmp = 0;
+  for(int i=0; i<8; i++){
+    if(amplitude[i]>highestAmp){
+      highestAmp = amplitude[i];  
+    }
+  } 
+  return highestAmp; 
 }
 
 int determineDirection(){
-  int totalFreq = 0;
+  int directionIndicator = 0;
   for(int i=0; i<8; i++){
     if((i==3|| i==4 ||i==5) && frequency[i]>500){//right
-      totalFreq += -1;
-    }else if(i == 6 && frequency[i]>500){//backward
-      totalFreq += 10;
+      directionIndicator += -1;
+     }else if(i == 6 && frequency[i]>500){//backward
+      directionIndicator += 1;
     }else if((i==0 ||i==1 || i == 7) && frequency[i]>500){ //left
-      totalFreq += 1;
+      directionIndicator += 1;
     }else if(i== 2 && frequency[i]>500){//front
-      totalFreq += 0;
+      directionIndicator += 0;
     }
   }
-  return totalFreq;    
+  return directionIndicator;    
 }
 
-void adjustDirection(int totalFreq){
-  if(totalFreq>0){
+int adjustDirection(int directionIndicator){
+  if(directionIndicator>0){
     bot.OLEDprint("", "Swarmy to the left", "");
-    bot.setMotorSpeed(0, 0);
-    bot.setMotorSpeed(1, 100);  
-  }else if(totalFreq<0){
-    bot.OLEDprint("","Swarmy to the right","");
-    bot.setMotorSpeed(0, 100);
-    bot.setMotorSpeed(1, 0);  
-  }else if(totalFreq == 0){
+    currentDirection = LEFT;
+    return 0;
+  }else if(directionIndicator<0){
+    bot.OLEDprint("","Swarmy to the right",""); 
+    currentDirection = RIGHT;
+    return 1;
+  }else if(directionIndicator == 0){
     bot.OLEDprint("", "Swarmy to the front", "");
-   // if(determineAmp()>100){
-      bot.setMotorSpeed(0, 0);
-      bot.setMotorSpeed(1, 0); 
-    //}//adjustPosition(determineAmp());
+    currentDirection = FORWARD;
+    return 2;
   }
 }
+
+void searchPattern(){
+  int randDirection = random(4);
+  
+  if(randDirection == 0){ 
+    bot.OLEDprint("", "Forward", "");
+    currentDirection = FORWARD;
+ }else if(randDirection == 1){
+    bot.OLEDprint("", "Forward Left", "");
+    currentDirection = FORWARD_LEFT;
+  }else if(randDirection == 2){
+    bot.OLEDprint("", "Backward", "");
+    currentDirection = BACKWARD;
+  }else if(randDirection == 3){
+    bot.OLEDprint("", "Forward Right", "");
+    currentDirection = FORWARD_RIGHT;
+  }     
+}
+
+float determineSpeed(float measuredAmp){
+  if(measuredAmp != desiredAmp){
+    float x = desiredAmp - measuredAmp;
+    float power = pow(e, 0.08*x);
+    float division = 1/(1+ power);
+    float equation = 1 - (division * 2);
+    return equation;
+  }else if(measuredAmp == 0){
+    return 1;
+  }else{
+    return 0;  
+  }
+ 
+}
+
 void driving(){
-  long randLong = random(4);
-  switch(randLong){
-    case 0:
-      bot.setMotorSpeed(0, -100);
-      bot.setMotorSpeed(1, 100);
-      delay(400); 
+  switch(currentDirection){
+    case LEFT:
+      bot.setMotorSpeed(0, -determineSpeed(determineAmplitude())*100);
+      bot.setMotorSpeed(1, determineSpeed(determineAmplitude())*25);
     break;
-    case 1:
-      bot.setMotorSpeed(0, 100);
-      bot.setMotorSpeed(1, -100);
-      delay(400);
+    
+    case RIGHT:
+      bot.setMotorSpeed(0, determineSpeed(determineAmplitude())*25);
+      bot.setMotorSpeed(1, -determineSpeed(determineAmplitude())*100);
     break;
-    case 2:
-      bot.setMotorSpeed(0, 100);
-      bot.setMotorSpeed(1, 100); 
+    
+    case FORWARD:
+      bot.setMotorSpeed(0, -determineSpeed(determineAmplitude())*100);
+      bot.setMotorSpeed(1, -determineSpeed(determineAmplitude())*100); 
+    break; 
+
+    case FORWARD_LEFT:
+      bot.setMotorSpeed(0, -determineSpeed(determineAmplitude())*100);
+      bot.setMotorSpeed(1, -determineSpeed(determineAmplitude())*50);
     break;
-    case 3:
-      bot.setMotorSpeed(0, -100);
-      bot.setMotorSpeed(1, -100); 
-    break;  
+
+    case FORWARD_RIGHT:
+      bot.setMotorSpeed(0, -determineSpeed(determineAmplitude())*50);
+      bot.setMotorSpeed(1, -determineSpeed(determineAmplitude())*100);
+    break;
+    
+    case BACKWARD:
+      bot.setMotorSpeed(0, determineSpeed(determineAmplitude())*100);
+      bot.setMotorSpeed(1, determineSpeed(determineAmplitude())*100); 
+    break;
+    
+    case NONE:
+      bot.setMotorSpeed(0, 0);
+      bot.setMotorSpeed(1, 0); 
+    break;
   }
 }
